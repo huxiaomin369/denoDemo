544fb23409ffcec033c8e3c3e12be25b2aa45824
mhu <694904679@qq.com.com>
2025-02-16 15:42:21 +0800

    add wechat verify

--- main.ts
+++ main.ts
@@ -3,15 +3,16 @@ import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts";
 const app = new Hono();
 const kv = await Deno.openKv();
 
-app.get("/", async (c) => {
-  const entries = [];
-  for await (const entry of kv.list({ prefix: [] })) {
-    entries.push({
-      userid: entry.key[0],
-      count: entry.value.toString()
-    });
-  }
-  return c.json(entries);
+app.get("/WW_verify_sLiFnl9UzNfQ9jxT.txt", async (c) => {
+  // const entries = [];
+  // for await (const entry of kv.list({ prefix: [] })) {
+  //   entries.push({
+  //     userid: entry.key[0],
+  //     count: entry.value.toString()
+  //   });
+  // }
+  // return c.json(entries);
+  return c.text('sLiFnl9UzNfQ9jxT');
 });
 
 app.get("/add/:userid", async (c) => {
